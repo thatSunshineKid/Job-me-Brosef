c8ec5ec919d8aef592d41a33bfbef930c86c7736
saasbook <saasbook@saasbook.(none)>
2013-11-06 16:46:46 -0800

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JobMeBrosef::Application.config.secret_token = '0918735219f5fb936c80912634e38541d87db79d4edb2e2accabcb30a2e2bc2bd51fa56099b398bfb34ad9cd53100a3c5df607c84723ab861bc79048e5feb47f'
