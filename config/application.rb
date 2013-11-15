16c4faf7494c4a97df04e5cc3ae4bc39635c99ec
Adam Sunshine <adam@csuac.com>
2013-11-15 16:51:39 -0700

    modified application.rb 2

--- config/application.rb
+++ config/application.rb
@@ -3,7 +3,7 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 
 
-config.assets.initialize_on_precompile = false
+
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
@@ -61,5 +61,7 @@ module JobMeBrosef
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.initialize_on_precompile = false
   end
 end
