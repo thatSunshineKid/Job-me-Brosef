259113377746488b77374b007069bbecec95a936
Adam Sunshine <adam@csuac.com>
2013-12-04 09:45:26 -0700

    add index validation

--- app/models/job_application.rb
+++ app/models/job_application.rb
@@ -1,6 +1,7 @@
 class JobApplication < ActiveRecord::Base
   attr_accessible :company_name, :company_response, :contact, :cover_letter_and_resume_sent, :cover_letter_completed, :job_title, :notes, :resume_completed
   belongs_to :user
+  validates :user_id, presence: true
   
   validates :company_name, presence: true, length: { maximum: 50 }
   validates :job_title, presence: true, length: { maximum: 50 }
