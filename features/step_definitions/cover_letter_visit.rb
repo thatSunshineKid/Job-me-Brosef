5cae951b193acae771fcb550836367f1bedc33c5
Adam Sunshine <adam@csuac.com>
2013-12-03 20:46:19 -0700

    cucumber change 4

--- features/step_definitions/cover_letter_visit.rb
+++ features/step_definitions/cover_letter_visit.rb
@@ -8,6 +8,6 @@ When /^they click the link to visit the cover letter page$/ do
 	visit '/static_pages/cover_letter_tips'
 end
 
-Then /^Then they should see "([^"]*)"/ do |content|
-	expect(page).to have_content content
+Then /^Then they should see "(.*?)"$/ do |content|
+	page.should have_content(content)
 end
