8cd3326587438ac527c0a92f9c896beb43347afb
Adam Sunshine <adam@csuac.com>
2013-12-03 18:25:14 -0700

    first try at cucumber 2

--- features/step_definitions/cover_letter_visit.rb
+++ features/step_definitions/cover_letter_visit.rb
@@ -1,11 +1,11 @@
 
 
 Given /^a user visits the cover letter help page$/ do
-	visit cover_letter_tips_path
+	visit '/'
 end
 
 When /^they click the link to visit the cover letter page$/ do
-	visit cover_letter_tips_path
+	visit '/static_pages/cover_letter_tips'
 end
 
 Then /^Then they should see the cover letter content/ do
