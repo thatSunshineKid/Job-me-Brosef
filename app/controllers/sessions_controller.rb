fae702d11abf97b8b66752c58b3076431a0abccc
Adam Sunshine <adam@csuac.com>
2013-12-05 19:27:34 -0700

    added sessions view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,20 @@
 class SessionsController < ApplicationController
+
+	def create
+		user = User.find_by(name: params[:session][:name])
+		if user && user.authenticate(params[:session][:password])
+			sign_in user
+			redirect_to user
+		else
+			flash[:error] = 'Invalid username/password combination'
+			render 'new'
+		end
+	end
+
+	def new
+	end
+
+	def destroy
+	end
+
 end
