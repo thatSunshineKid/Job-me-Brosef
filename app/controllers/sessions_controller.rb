4fc9ac349d0e98a1b23b291a4490ee5aec2a70ab
Adam Sunshine <adam@csuac.com>
2013-12-06 01:16:09 -0700

    fixed signin signout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,22 +1,23 @@
 class SessionsController < ApplicationController
 
-	def create
-	  user = User.find_by_name(params[:session][:name])
-	  if user && user.authenticate(params[:session][:password])
-		sign_in user
-		redirect_to user
-	  else
-		flash[:error] = 'Invalid username/password combination'
-		render 'new'
-	  end
+  def create
+	user = User.find_by_name(params[:session][:name])
+	if user && user.authenticate(params[:session][:password])
+	  sign_in user
+	  redirect_to user
+	else
+	  flash[:error] = 'Invalid username/password combination'
+	  render 'new'
 	end
+  end
 
-	def new
-	end
+  def new
+	
+  end
 
-	def destroy
-	  sign_out
-	  redirect_to root_url
-	end
+  def destroy
+	sign_out
+	redirect_to root_url
+  end
 
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,9 +2,7 @@ module SessionsHelper
   
 
   def sign_in(user)
-    remember_token = User.new_remember_token
-    cookies.permanent[:remember_token] = remember_token
-    user.update_attribute(:remember_token, User.encrypt(remember_token))
+    cookies.permanent[:remember_token] = user.remember_token
     self.current_user = user
   end
 
@@ -13,8 +11,7 @@ module SessionsHelper
   end
 
   def current_user
-  	remember_token = User.encrypt(cookies[:remember_token])
-  	@current_user ||= User.find_by(remember_token: remember_token)
+    @current_user ||=User.find_by_remember_token(cookies[:remember_token])
   end
 
   def signed_in?
