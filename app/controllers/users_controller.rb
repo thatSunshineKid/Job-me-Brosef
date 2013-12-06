252112ab609a37417b35bc37a93093a73ab47ac6
Adam Sunshine <adam@csuac.com>
2013-12-06 04:31:51 -0700

    d

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,9 +31,10 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     if @user.save
       sign_in @user
-      flash[:success] = 'Welcome to the job thing!'
+      flash.now[:success] = 'Welcome to the job thing!'
       redirect_to @user
     else
+      flash.now[:error] = 'invalid username/password'
       render 'new'
     end
   end
