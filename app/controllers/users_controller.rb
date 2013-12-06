fb400a3d3d8a16181692c9b71eb53e0bb37c6f25
Adam Sunshine <adam@csuac.com>
2013-12-05 18:19:40 -0700

    waffle3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,7 +47,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        @job_applications = user.job_applications
+        @job_applications = @user.job_applications
         format.html { redirect_to @job_applications, notice: 'User was successfully created.' }
         format.json { render json: @job_applications, status: :created, location: @job_applications }
       else
