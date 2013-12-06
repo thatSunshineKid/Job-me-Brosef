ee44f6b7946a8fca34128c3dbc10aa20be00c975
Adam Sunshine <adam@csuac.com>
2013-12-05 19:49:49 -0700

    added signin/signout

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+
+  before_create :create_remember_token
+
   attr_accessible :name, :password, :password_confirmation
   validates :name, presence: true, length: { maximum: 20}, uniqueness: { case_sensitive: false }
 
@@ -6,4 +9,19 @@ class User < ActiveRecord::Base
   has_secure_password
 
   has_many :job_applications
+
+  def User.new_remember_token
+  	SecureRandom.urlsafe_base64
+  end
+
+
+  def User.encrypt(token)
+  	Digest::SHA1.hexidigest(token.to_s)
+  end
+
+  private
+
+  	def create_remember_token
+  	  self.remember_token = User.encrypt(User.new_remember_token)
+  	end
 end
