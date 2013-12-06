0f12fd66ed7486321b1c7954e327bd67de58cf70
Adam Sunshine <adam@csuac.com>
2013-12-06 04:34:23 -0700

    dkjdj

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 	user = User.find_by_name(params[:session][:name])
 	if user && user.authenticate(params[:session][:password])
 	  sign_in user
-	  redirect_back_or user
+	  redirect_to user
 	else
 	  flash.now[:error] = 'Invalid username/password combination'
 	  render 'new'
