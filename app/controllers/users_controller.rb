2d0b363d570e7e48afd62dee6543427eee981c8c
Adam Sunshine <adam@csuac.com>
2013-12-06 02:47:12 -0700

    sdfsa

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,6 @@ class UsersController < ApplicationController
   before_filter :singed_in_user, only: [:edit, :update]
   before_filter :correct_user, only: [:edit, :update]
 
-
-
   # GET /users
   # GET /users.json
   def index
@@ -15,23 +13,12 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
 
-
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @user }
-    end
   end
 
   # GET /users/new
   # GET /users/new.json
   def new
     @user = User.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @user }
-    end
   end
 
   # GET /users/1/edit
@@ -70,10 +57,6 @@ class UsersController < ApplicationController
   def destroy
     @user = User.find(params[:id])
     @user.destroy
-
-    respond_to do |format|
-      format.html { redirect_to users_url }
-      format.json { head :no_content }
     end
   end
 
@@ -91,4 +74,3 @@ class UsersController < ApplicationController
       redirect_to(root_url) unless current_user?(@user)
     end
     
-end
