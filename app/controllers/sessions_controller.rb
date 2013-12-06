eeafbf0b6f0dbb27f5e06ab5f8b161e1b0da1ba6
Adam Sunshine <adam@csuac.com>
2013-12-05 19:55:17 -0700

    bug fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
 
 	def create
-		user = User.find_by(name: params[:session][:name])
+		user = User.find(name: params[:session][:name])
 		if user && user.authenticate(params[:session][:password])
 			sign_in user
 			redirect_to user
