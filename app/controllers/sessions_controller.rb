f98a6a73ff2d7e377a4e6eb0ac18814bd0ea092f
Adam Sunshine <adam@csuac.com>
2013-12-05 20:06:22 -0700

    sdf

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,14 @@
 class SessionsController < ApplicationController
 
 	def create
-		user = User.find_by_name(params[:session][:name])
-		if user && user.authenticate(params[:session][:password])
-			sign_in user
-			redirect_to user
-		else
-			flash[:error] = 'Invalid username/password combination'
-			render 'new'
-		end
+	  user = User.find_by_name(params[:session][:name])
+	  if user && user.authenticate(params[:session][:password])
+		sign_in user
+		redirect_to user
+	  else
+		flash[:error] = 'Invalid username/password combination'
+		render 'new'
+	  end
 	end
 
 	def new
