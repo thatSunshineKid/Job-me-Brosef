6efb076685a9df73832b4ce9c8722c3b43b254ba
Adam Sunshine <adam@csuac.com>
2013-12-08 01:44:49 -0700

    sdfs

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 	user = User.find_by_name(params[:session][:name])
 	if user && user.authenticate(params[:session][:password])
 	  sign_in user
-	  redirect_to "/job_applications"
+	  redirect_to user
 	else
 	  flash.now[:error] = 'Invalid username/password combination'
 	  render 'new'
@@ -21,3 +21,6 @@ class SessionsController < ApplicationController
   end
 
 end
+
+
+
