5a7e9b4cbe17fcf415ee048c46b07888ba46ea7b
Adam Sunshine <adam@csuac.com>
2013-12-11 09:44:39 -0700

    sdf

--- spec/routes/job_application_spec.rb
+++ spec/routes/job_application_spec.rb
@@ -2,14 +2,14 @@ require 'spec_helper'
 
 describe StaticPagesController, :type => :controller do
   it "works for cover letter page" do
-    get("/cover_letter_tips").should route_to("static_pages#cover_letter_tips")
+    get("cover_letter_tips").should route_to("static_pages#cover_letter_tips")
   end
 
   it "works for home page" do
-  	get("/").should route_to("static_pages#home")
+  	get("").should route_to("static_pages#home")
   end
 
   it "works for helpful links" do
-  	get("/helpful_links").should route_to("static_pages#helpful_links")
+  	get("helpful_links").should route_to("static_pages#helpful_links")
   end
 end
