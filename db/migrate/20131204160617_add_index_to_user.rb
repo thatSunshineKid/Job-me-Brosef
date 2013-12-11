4a94abf467659e678aa93113138f9b666afd4bbd
Adam Sunshine <adam@csuac.com>
2013-12-11 10:27:24 -0700

    sdd

--- db/migrate/20131204160617_add_index_to_user.rb
+++ db/migrate/20131204160617_add_index_to_user.rb
@@ -1,5 +1,5 @@
 class AddIndexToUser < ActiveRecord::Migration
   def change
-    add_column :job_applications, :user_id, :string
+    add_column :job_applications, :user_id, :integer
   end
 end
--- db/migrate/20131211170928_change_user_idto_int.rb
+++ db/migrate/20131211170928_change_user_idto_int.rb
@@ -1,5 +1,4 @@
 class ChangeUserIdtoInt < ActiveRecord::Migration
   def change
-  	change_column :job_applications, :user_id, :integer
   end
 end
