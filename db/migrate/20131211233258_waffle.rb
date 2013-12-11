f843285108974c21de5e8c7918dfc2135b328b6c
Adam Sunshine <adam@csuac.com>
2013-12-11 16:59:19 -0700

    sdf

--- db/migrate/20131211233258_waffle.rb
+++ db/migrate/20131211233258_waffle.rb
@@ -1,6 +1,2 @@
 class Waffle < ActiveRecord::Migration
-  def up
- 	sql = "ALTER TABLE job_applications ALTER COLUMN user_id SET DATA TYPE integer;"
- 	ActiveRecord::Base.connection.execute(sql)
-  end
 end
--- db/migrate/20131211235230_fix_job_app.rb
+++ db/migrate/20131211235230_fix_job_app.rb
@@ -1,7 +1,21 @@
 class FixJobApp < ActiveRecord::Migration
   def up
+  	create_table :job_applications do |t|
+      t.string :company_name
+      t.string :job_title
+      t.string :contact
+      t.boolean :cover_letter_completed
+      t.boolean :resume_completed
+      t.boolean :cover_letter_and_resume_sent
+      t.boolean :company_response
+      t.string :notes
+      t.integer :user_id
+      
+      t.timestamps
+    end
   end
 
   def down
+  	drop_table :job_applications
   end
 end
