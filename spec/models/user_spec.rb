d7dca96bef69dc59250a673abeef21e00de70b0b
Adam Sunshine <adam@csuac.com>
2013-12-11 09:18:16 -0700

    sdf

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,7 +6,15 @@ describe User do
     user.name.should match(/Anna/)	
   end
 
+  it "changes the number of users" do
+  	user = User.new(name: 'bobby', password: "123456")
+  	expect { user.save }.to change { User.count }.by(1)
+  end
+  it "should not have any jobs to start" do
+  	user = User.new(name: 'bob')
+  	jobs = user.job_applications.all
+  	jobs.count should_match(0)
+  end
 
-  
 end
 
