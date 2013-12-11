5cf500d9607e5fbfd52c86002f6e48d18a04dfaf
Adam Sunshine <adam@csuac.com>
2013-12-10 22:54:17 -0700

    sdfa

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    if signed_in?
+    if current_user != nil
       @user = current_user
     else
       flash.now[:error] = 'Pleas sign in first.'
