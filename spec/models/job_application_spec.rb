87890bccc4641e69b257ff2d97c13cc462eb2a27
Adam Sunshine <adam@csuac.com>
2013-12-11 09:08:39 -0700

    sdf

--- spec/models/job_application_spec.rb
+++ spec/models/job_application_spec.rb
@@ -1,6 +1,5 @@
-describe User do
-  it "has a name that matches 'Anna'" do
-    user = User.new(name: 'Anna')
-    user.name.should match(/Anna /)	
-  end
+require 'spec_helper'
+
+describe JobApplication do
+  
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 describe User do
   it "has a name that matches 'Anna'" do
     user = User.new(name: 'Anna')
-    user.name.should match(/Anna /)	
+    user.name.should match(/Anna/)	
   end
+
+
+  
 end
+
