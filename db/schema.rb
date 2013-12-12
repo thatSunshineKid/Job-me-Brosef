ac900ac66dc2cb0b98801407aba420840bbb0d17
saasbook <saasbook@saasbook.(none)>
2013-12-11 16:07:51 -0800

    adj schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131211173629) do
+ActiveRecord::Schema.define(:version => 20131212000331) do
 
   create_table "job_applications", :force => true do |t|
     t.string   "company_name"
@@ -22,9 +22,9 @@ ActiveRecord::Schema.define(:version => 20131211173629) do
     t.boolean  "cover_letter_and_resume_sent"
     t.boolean  "company_response"
     t.string   "notes"
-    t.datetime "created_at",                                  :null => false
-    t.datetime "updated_at",                                  :null => false
-    t.integer  "user_id",                      :limit => 255
+    t.integer  "user_id"
+    t.datetime "created_at",                   :null => false
+    t.datetime "updated_at",                   :null => false
   end
 
   create_table "users", :force => true do |t|
